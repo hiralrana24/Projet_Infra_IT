0ea024d01f62ba08f0dbf89b31bc0e3876689f2e
hiralrana24 <hiral.rana0910@gmail.com>
2026-01-21 11:16:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,6 +12,10 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie(): #Comm2
     return session.get('authentifie')
 
+def est_authentifie_user():
+    return session.get('authentifie_user')
+
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
